dd8f27229bc30ce17729d3b98a6f930173de634b
kanza.latif <kanzalateef@gmail.com>
2025-01-30 10:55:13 +0500

    added command for interface root_guard

--- config/stp.py
+++ config/stp.py
@@ -1058,45 +1058,45 @@ def spanning_tree_interface(_db):
     pass
 
 
-# config spanning_tree interface enable <interface_name>
-# MST CONFIGURATION IN THE STP_PORT TABLE
-# It will have a check for global stp mode to figure out for which mode is it working?
-@spanning_tree_interface.command('enable')
-@click.argument('interface_name', metavar='<interface_name>', required=True)
-@clicommon.pass_db
-def stp_interface_enable(_db, interface_name):
-    """Enable STP for interface"""
-    ctx = click.get_current_context()
-    db = _db.cfgdb
-    check_if_global_stp_enabled(db, ctx)
-    if is_stp_enabled_for_interface(db, interface_name):
-        ctx.fail("STP is already enabled for " + interface_name)
-    check_if_interface_is_valid(ctx, db, interface_name)
-    stp_intf_entry = db.get_entry('STP_PORT', interface_name)
-    if len(stp_intf_entry) == 0:
-        fvs = {'enabled': 'true',
-               'root_guard': 'false',
-               'bpdu_guard': 'false',
-               'bpdu_guard_do_disable': 'false',
-               'portfast': 'false',
-               'uplink_fast': 'false'}
-        db.set_entry('STP_PORT', interface_name, fvs)
-    else:
-        db.mod_entry('STP_PORT', interface_name, {'enabled': 'true'})
+# # config spanning_tree interface enable <interface_name>
+# # MST CONFIGURATION IN THE STP_PORT TABLE
+# # It will have a check for global stp mode to figure out for which mode is it working?
+# @spanning_tree_interface.command('enable')
+# @click.argument('interface_name', metavar='<interface_name>', required=True)
+# @clicommon.pass_db
+# def stp_interface_enable(_db, interface_name):
+#     """Enable STP for interface"""
+#     ctx = click.get_current_context()
+#     db = _db.cfgdb
+#     check_if_global_stp_enabled(db, ctx)
+#     if is_stp_enabled_for_interface(db, interface_name):
+#         ctx.fail("STP is already enabled for " + interface_name)
+#     check_if_interface_is_valid(ctx, db, interface_name)
+#     stp_intf_entry = db.get_entry('STP_PORT', interface_name)
+#     if len(stp_intf_entry) == 0:
+#         fvs = {'enabled': 'true',
+#                'root_guard': 'false',
+#                'bpdu_guard': 'false',
+#                'bpdu_guard_do_disable': 'false',
+#                'portfast': 'false',
+#                'uplink_fast': 'false'}
+#         db.set_entry('STP_PORT', interface_name, fvs)
+#     else:
+#         db.mod_entry('STP_PORT', interface_name, {'enabled': 'true'})
 
 
-# config spanning_tree interface disable <interface_name>
-# MST CONFIGURATION IN THE STP_PORT TABLE
-@spanning_tree_interface.command('disable')
-@click.argument('interface_name', metavar='<interface_name>', required=True)
-@clicommon.pass_db
-def stp_interface_disable(_db, interface_name):
-    """Disable STP for interface"""
-    ctx = click.get_current_context()
-    db = _db.cfgdb
-    check_if_global_stp_enabled(db, ctx)
-    check_if_interface_is_valid(ctx, db, interface_name)
-    db.mod_entry('STP_PORT', interface_name, {'enabled': 'false'})
+# # config spanning_tree interface disable <interface_name>
+# # MST CONFIGURATION IN THE STP_PORT TABLE
+# @spanning_tree_interface.command('disable')
+# @click.argument('interface_name', metavar='<interface_name>', required=True)
+# @clicommon.pass_db
+# def stp_interface_disable(_db, interface_name):
+#     """Disable STP for interface"""
+#     ctx = click.get_current_context()
+#     db = _db.cfgdb
+#     check_if_global_stp_enabled(db, ctx)
+#     check_if_interface_is_valid(ctx, db, interface_name)
+#     db.mod_entry('STP_PORT', interface_name, {'enabled': 'false'})
 
 
 # STP interface port priority
@@ -1661,9 +1661,9 @@ def stp_interface_enable(_db, interface_name):
 
     if is_stp_enabled_for_interface(db, interface_name):
         ctx.fail("STP is already enabled for " + interface_name)
-    
+
     check_if_interface_is_valid(ctx, db, interface_name)
-    
+
     stp_intf_entry = db.get_entry('STP_PORT', interface_name)
     mode = get_global_stp_mode(db)
 
@@ -1671,13 +1671,13 @@ def stp_interface_enable(_db, interface_name):
            'root_guard': 'false',
            'bpdu_guard': 'false',
            'bpdu_guard_do_disable': 'false'}
-    
+
     if mode == "pvst":
         fvs.update({'portfast': 'false',
                     'uplink_fast': 'false'})
     elif mode == "mst":
         fvs.update({'edge_port': 'false',
-                    'link_type': 'auto'}) # Adjusted values based on HLD
+                    'link_type': 'auto'})
 
     if len(stp_intf_entry) == 0:
         db.set_entry('STP_PORT', interface_name, fvs)
