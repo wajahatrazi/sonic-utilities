f50d16574c7c37316f2e1742c1f24861036641ef
kanza.latif <kanzalateef@gmail.com>
2025-02-04 10:54:28 +0500

    updated enable disable functions

--- tests/stp_test.py
+++ tests/stp_test.py
@@ -176,7 +176,7 @@ class TestStp(object):
             2,
             "Current STP mode: none\nUsage: enable [OPTIONS] <interface_name>\n"
             "Try 'enable --help' for help.\n\nError: Global STP mode is not enabled\n"
-        )
+        ),
         # Enable pvst
         (config.config.commands["spanning-tree"].commands["enable"], ["pvst"], 0, None),
         # Configure interface priority and cost
